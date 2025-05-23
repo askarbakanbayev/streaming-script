d47c37bcb896fd59773bba5eb80245dd11bff231
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-23 11:43:34 +0500

    feat: add steaming

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StreamingModule } from './streaming/streaming.module';
 
 @Module({
-  imports: [],
+  imports: [StreamingModule],
   controllers: [AppController],
   providers: [AppService],
 })
