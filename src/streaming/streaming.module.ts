071ab0858815d327902362f6922282a3887ffab8
Erlan-Bam <erlanzh.gg@gmail.com>
2025-05-23 12:32:41 +0500

    feat: updated

--- src/streaming/streaming.module.ts
+++ src/streaming/streaming.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StreamsController } from './streaming.controller';
 import { StreamsService } from './streaming.service';
+import { BotModule } from 'src/bot/bot.module';
 
 @Module({
+  imports: [BotModule],
   controllers: [StreamsController],
   providers: [StreamsService],
 })
